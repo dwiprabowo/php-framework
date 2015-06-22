c776cf5335e2e66e40995f24faf893e8bd872d92
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2015-06-23 04:34:47 +0700

    update localhost, add vim

---
+++ application/controllers/Vim.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Vim extends Web_Controller{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    function index(){}
+}
