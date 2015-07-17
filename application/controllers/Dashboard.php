9eddcb9b65621b66d5da60a883b85c2a58532510
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2015-07-17 22:24:19 +0700

    update dashboard & view

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -7,9 +7,57 @@ class Dashboard extends Auth_Controller{
         parent::__construct();
     }
 
-    public function index(){}
+    function index(){
+        $this->_var(
+            'users_count', 
+            [
+                'total' => $this->user_model->count_all(),
+                'active' => $this->user_model->count_by(['active' => 1]),
+                'inactive' => $this->user_model->count_by(['active' => 0]),
+            ]
+        );
+        $this->_var('master_count', 
+            [
+                'total' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_MASTER
+                ]),
+                'active' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_MASTER, 'active' => 1
+                ]),
+                'inactive' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_MASTER, 'active' => 0
+                ]),
+            ]
+        );
+        $this->_var('admin_count', 
+            [
+                'total' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_ADMIN
+                ]),
+                'active' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_ADMIN, 'active' => 1
+                ]),
+                'inactive' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_ADMIN, 'active' => 0
+                ]),
+            ]
+        );
+        $this->_var('user_count', 
+            [
+                'total' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_USER
+                ]),
+                'active' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_USER, 'active' => 1
+                ]),
+                'inactive' => $this->user_model->count_by([
+                    'role' => User_Model::ROLE_USER, 'active' => 0
+                ]),
+            ]
+        );
+    }
 
     function _models(){
-        return ['login'];
+        return ['login', 'user'];
     }
 }
