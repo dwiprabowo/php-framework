ca479c06980be2bf49b00b69e343ca5cab4a896c
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2015-11-22 10:41:00 +0700

    new download link android client

--- constants.php
+++ constants.php
@@ -4,3 +4,7 @@
 define('TWBS', 'vendor/twbs/bootstrap/');
 define('PRECONFIG', __DIR__.DS.'preconfig'.EXT);
 define('VENDOR', __DIR__.DS.'vendor/autoload'.EXT);
+define(
+    'ANDROID_DOWNLOAD'
+    , 'https://drive.google.com/file/d/0B2z9x0nzjPJtaXcyQ2ZfREEwY2s/view?usp=sharing'
+);
